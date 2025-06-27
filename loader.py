a87ce768d61fd21a9e27a28e3375af4f6da4f1d7
brkim <br26kim@gmail.com>
2025-06-27 11:50:04 +0900

    update

--- loader.py
+++ loader.py
@@ -1,9 +1,28 @@
 from basic import *
 logger = logging.getLogger(name = __name__)
 
+import pandas as pd
+
 
 class Loader:
     def __init__(self):
         pass
     def __repr__(self):
         return 'loader'
+
+    def load(self, name, train = True, normal = True):
+        if not isinstance(name, str):
+            raise TypeError('The name should be a string.')
+        if not isinstance(train, bool):
+            raise TypeError('\'train\' should be boolean.')
+        if not isinstance(normal, bool):
+            raise TypeError('\'normal\' should be boolean.')
+        if train:
+            kind = 'train'
+        else:
+            kind = 'test'
+        if normal:
+            label = 'benign'
+        else:
+            label = 'attack'
+        # To be implemented.
